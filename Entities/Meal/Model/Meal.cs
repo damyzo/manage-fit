0918b17ff67d0ca24688dc24dbdd24a17cf37fb2
damyzo <ilievskidamjan4@gmail.com>
2024-08-21 00:29:05 +0200

    Add class models

--- Entities/Meal/Model/Meal.cs
+++ Entities/Meal/Model/Meal.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entities.Meal.Model
+﻿namespace Entities.Meal.Model
 {
     public class Meal
     {
