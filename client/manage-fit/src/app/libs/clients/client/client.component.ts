94556c3165ee5f9229a1b4c9f880e2ea070fd370
damyzo <ilievskidamjan4@gmail.com>
2025-05-29 00:40:56 +0200

    Remove unused import.

--- client/manage-fit/src/app/libs/clients/client/client.component.ts
+++ client/manage-fit/src/app/libs/clients/client/client.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
